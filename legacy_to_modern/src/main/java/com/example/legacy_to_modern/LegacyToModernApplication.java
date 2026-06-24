27cfe423a0a17e13154f3dddd554476cbd7f8672
cholholim <ghfla225@gmail.com>
2026-06-25 00:09:36 +0900

    feat: SQL 인젝션 취약점 방어, 전역 예외 처리 적용 및 DB 초기화 자동화"

---
+++ legacy_to_modern/src/main/java/com/example/legacy_to_modern/LegacyToModernApplication.java
@@ -0,0 +1,13 @@
+package com.example.legacy_to_modern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyToModernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyToModernApplication.class, args);
+	}
+
+}
---
+++ legacy_to_modern/src/test/java/com/example/legacy_to_modern/LegacyToModernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.legacy_to_modern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyToModernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
